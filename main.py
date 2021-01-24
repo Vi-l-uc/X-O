1e6de539be2ab24726a66e1bed563d0ced98e04d
Vi-l-uc <viluc@bk.ru>
2021-01-24 22:21:18 +0300

    Создание поля

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-print('Hello!!!')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Игра в крестики - нолики. \n"
+      "Первым ходят крестики. \n"
+      "Введите номер столбца, потом строки.")
+pole = (f'   -------------\n'
+        f' 2 |   |   |   |\n'
+        f'   -------------\n'
+        f' 1 |   |   |   |\n'
+        f'   -------------\n'
+        f' 0 |   |   |   |\n'
+        f'   -------------\n'
+        f'     0   1   2'
+        )
+print(pole)
