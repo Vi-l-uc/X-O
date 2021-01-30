7e862e079a365d763ebfc1c8813c93bd6f8699f0
Vi-l-uc <viluc@bk.ru>
2021-01-30 23:24:11 +0300

    Без условия победы

--- main.py
+++ main.py
@@ -4,29 +4,56 @@ print("Игра в крестики - нолики. \n"
       "Первым ходят крестики. \n")
 
 
-def chis(x):
-    x1 = int(x)-1
-    X = [i for i in range(9)]
-    if x1 in X:
-        return x1
+def chis(p):
+    X = [i for i in range(1, 10)]
+    if p.isdigit():
+        if int(p) in X:
+            return int(p)
+        else:
+            print('Введите число от 1 до 9')
+            return pole()
     else:
-        print('Введите чесло от 1 до 9')
+        print('Введите число от 1 до 9')
+        return pole()
 
 
-p = input('Введите номер ячейки: ')
+def hod(x):
+    global P
+    if P[x] == ' ':
+        return x
+    else:
+        print("Ячейка занята, попробуйте снова!")
+        return pole()
 
-P = {i: ' ' for i in range(9)}
-P[chis(p)] = "X"
 
-pole = (f'   +---+---+---+\n'
-        f'   | {P[0]}¹| {P[1]}²| {P[2]}³|\n'
-        f'   +---+---+---+\n'
-        f'   | {P[3]}⁴| {P[4]}⁵| {P[5]}⁶|\n'
-        f'   +---+---+---+\n'
-        f'   | {P[6]}⁷| {P[7]}⁸| {P[8]}⁹|\n'
-        f'   +---+---+---+\n'
-        f'              '
-        )
-print(pole)
+def pole():
+    p = input('Введите номер ячейки: ')
+    return hod(chis(p))
+
+
+def game(pol_gam):
+    global P
+    print(pol_gam)
+    P[pole()] = "X"
+    print(pol_gam)
+    for i in range(4):
+        P[pole()] = "0"
+        print(pol_gam)
+        print(P)
+        P[pole()] = "X"
+        print(pol_gam)
+
+
+P = {i: ' ' for i in range(1, 10)}
+pole_game = (f'   +---+---+---+\n'
+             f'   | {P[1]}¹| {P[2]}²| {P[3]}³|\n'
+             f'   +---+---+---+\n'
+             f'   | {P[4]}⁴| {P[5]}⁵| {P[6]}⁶|\n'
+             f'   +---+---+---+\n'
+             f'   | {P[7]}⁷| {P[8]}⁸| {P[9]}⁹|\n'
+             f'   +---+---+---+\n'
+             f' ')
+
+game(pole_game)
 print(P)
-
+print(pole_game)
